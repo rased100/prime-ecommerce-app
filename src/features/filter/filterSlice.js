c1539185d7ad4ed5e126eb301f83798db5fc3ed3
rased <rased100@gmail.com>
2024-04-05 12:04:40 +0600

    category filter

--- src/features/filter/filterSlice.js
+++ src/features/filter/filterSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   stock: false,
   brand: [],
+  category: [],
   keywords: "",
 };
 
@@ -13,13 +14,18 @@ const filterSlice = createSlice({
     toggle: (state) => {
       state.stock = !state.stock;
     },
-    toggleBrands: (state, action) => {
+    brands: (state, action) => {
       const selectedBrand = action.payload;
       state.brand = selectedBrand !== state.brand ? selectedBrand : "";
+    },
+    categories: (state, action) => {
+      const selectedcategory = action.payload;
+      state.category =
+        selectedcategory !== state.category ? selectedcategory : "";
     },
   },
 });
 
-export const { toggle, toggleBrands } = filterSlice.actions;
+export const { toggle, brands, categories } = filterSlice.actions;
 
 export default filterSlice.reducer;
--- src/pages/Dashboard/ProductList.js
+++ src/pages/Dashboard/ProductList.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleBrands } from "../../features/filter/filterSlice";
+import { brands, categories } from "../../features/filter/filterSlice";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
 
   const dispatch = useDispatch();
   const filter = useSelector((state) => state.filter);
-  const { brand, stock } = filter;
+  const { brand, category } = filter;
 
   useEffect(() => {
     fetch("https://dummyjson.com/products?skip=0&limit=100")
@@ -26,11 +26,21 @@ const ProductList = () => {
         }
         return true;
       })
+      .filter((product) => {
+        if (category.length) {
+          return category.includes(product.category);
+        }
+        return true;
+      })
+
       .map((product) => <ProductCard key={product.model} product={product} />);
   }
 
-  const handleBrandToggle = (selectedBrand) => {
-    dispatch(toggleBrands(selectedBrand));
+  const handleBrands = (selectedBrand) => {
+    dispatch(brands(selectedBrand));
+  };
+  const handleCategories = (selectedcategory) => {
+    dispatch(categories(selectedcategory));
   };
 
   return (
@@ -38,16 +48,28 @@ const ProductList = () => {
       <div className="mb-10 flex justify-end gap-5">
         <select
           className="border px-3 py-2 rounded-full font-semibold"
-          onChange={(e) => handleBrandToggle(e.target.value)}
+          onChange={(e) => handleBrands(e.target.value)}
         >
-          <option value="">All Brands</option>
+          <option>Select Brands</option>
           <option value="Apple">Apple</option>
           <option value="Samsung">Samsung</option>
           <option value="OPPO">OPPO</option>
           <option value="Huawei">Huawei</option>
           <option value="Microsoft Surface">Microsoft</option>
-          <option value="Infinix">Infinix</option>
-          {/* Add more options for other brands if needed */}
+        </select>
+      </div>
+      {/* categories */}
+      <div className="mb-10 flex justify-end gap-5">
+        <select
+          className="border px-3 py-2 rounded-full font-semibold"
+          onChange={(e) => handleCategories(e.target.value)}
+        >
+          <option>Select Categories</option>
+          <option value="smartphones">Smartphones</option>
+          <option value="laptops">Laptops</option>
+          <option value="fragrances">Fragrances</option>
+          <option value="skincare">Skincare</option>
+          <option value="groceries">Groceries</option>
         </select>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
