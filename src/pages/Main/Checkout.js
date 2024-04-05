b6de25b4502ab50429e4a342ba7a659be17c09ef
rased <rased100@gmail.com>
2024-04-05 21:17:43 +0600

    checkout

--- src/pages/Main/Checkout.js
+++ src/pages/Main/Checkout.js
@@ -43,7 +43,7 @@ const Checkout = () => {
                 </div>
               </div>
               <button
-                onClick={() => handleRemoveFromCart(item.id)}
+                onClick={() => handleRemoveFromCart(item)}
                 className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-b-lg font-semibold"
               >
                 Remove
